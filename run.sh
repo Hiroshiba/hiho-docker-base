567bf5dc6b028547f8b6614118fa28a6ab709d6a
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2020-02-06 17:04:54 +0900

    v3

--- run.sh
+++ run.sh
@@ -2,8 +2,17 @@ set -eu
 
 target=${1:-'*'}
 
-for dockerfile in Dockerfile_$target; do
+target_files=$(ls Dockerfile_$target | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2-)
+
+for dockerfile in $target_files; do
   tag=${dockerfile/Dockerfile_/}
   docker build -f $dockerfile -t hiroshiba/hiho-docker-base:$tag .
-  docker push "hiroshiba/hiho-docker-base:$tag"
+  docker push hiroshiba/hiho-docker-base:$tag
 done
+
+if [ "$target" = '*' ]; then
+  dockerfile=$(echo "$target_files" | head -n1)
+  tag=${dockerfile/Dockerfile_/}
+  docker tag hiroshiba/hiho-docker-base:$tag hiroshiba/hiho-docker-base:latest
+  docker push hiroshiba/hiho-docker-base:latest
+fi
